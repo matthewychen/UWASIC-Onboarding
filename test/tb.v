5ac046dd032524640a853a842052050b974eabf7
Matthew Chen <matthewychen06@gmail.com>
2025-05-09 18:00:27 -0400

    removed test_mode from tb level

--- test/tb.v
+++ test/tb.v
@@ -44,8 +44,7 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n),     // not reset
-      .test_mode(test_mode)
+      .rst_n  (rst_n)     // not reset
   );
 
 endmodule
