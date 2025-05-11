8d39d1d9276dff1f8ebeb6484013de042c42ab62
Matthew Chen <matthewychen06@gmail.com>
2025-05-11 19:55:02 -0400

    removed comma

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -12,7 +12,7 @@ module spi_peripheral #(
     output reg [7:0] en_reg_out_15_8, 
     output reg [7:0] en_reg_pwm_7_0,
     output reg [7:0] en_reg_pwm_15_8,
-    output reg [7:0] pwm_duty_cycle,
+    output reg [7:0] pwm_duty_cycle
 );
 
 reg [4:0] curr_bit; 
