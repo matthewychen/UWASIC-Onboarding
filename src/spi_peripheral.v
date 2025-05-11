188fa809094a899ce6923f121a30f5443e3508c3
matthewychen <matthewychen06@gmail.com>
2025-05-11 16:28:10 -0400

    refactored spi module.

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -1,44 +1,40 @@
-`default_nettype none
+module spi_peripheral #(
+    parameter MAX_ADDR = 4,
+    parameter SYNC_FLOPS = 2
+)
+(
+    input wire       SCLK,// clock
+    input wire       COPI,//in from controller
+    input wire       nCS,//start transaction on negedge
+    input wire       clk,//
+    input wire       rst_n,// 
 
-module spi_peripheral #(parameter SYNC = 2)(
-    //SPI inputs
-    input wire nCS, 
-    input wire SCLK, 
-    input wire COPI,
-
-    //sysclk input
-    input wire clk,
-    input wire rst_n,        //active LOW
-
-    //outputs
-    output reg [7:0] en_reg_out_7_0,
-    output reg [7:0] en_reg_out_15_8,
-    output reg [7:0] en_reg_pwm_7_0,
-    output reg [7:0] en_reg_pwm_15_8,
-    output reg [7:0] pwm_duty_cycle
+    output wire [7:0] en_reg_out_7_0,
+    output wire [7:0] en_reg_out_15_8, 
+    output wire [7:0] en_reg_pwm_7_0,
+    output wire [7:0] en_reg_pwm_15_8,
+    output wire [7:0] pwm_duty_cycle,
+    output wire [2:0] addr_out
 );
 
-//internal regs
-reg [4:0] sCLKcnt;
-reg [15:0] data;
+reg [3:0] curr_bit; 
+reg transaction_done;
+reg [15:0] transaction_data;
 
-reg [SYNC-1:0] sync_nCS;
-reg [SYNC-1:0] sync_SCLK; 
-reg [SYNC-1:0] sync_COPI;
+reg [SYNC_FLOPS-1:0] SCLK_sync;
+reg [SYNC_FLOPS-1:0] COPI_sync;
+reg [SYNC_FLOPS-1:0] nCS_sync;
 
-//SPI DATA [15:0] IS [DATA 8 bit][ADDR 7 bit][R/W]
-//note the ADDR is by default 0x00-0x04
-
-//take in SPI data, 
 always @(posedge clk or negedge rst_n) begin
 
-    if (!rst_n) begin
-        sCLKcnt <= 5'b0;
-        data <= 16'b0;
+    if (!rst_n) begin //reset state
+        curr_bit <= 4'b0;
+        transaction_done <= 1'b0;
+        transaction_data <= 16'b0;
 
-        sync_nCS <= {SYNC{1'b0}};
-        sync_SCLK <= {SYNC{1'b0}};
-        sync_COPI <= {SYNC{1'b0}};
+        SCLK_sync <= 0;
+        COPI_sync <= 0;
+        nCS_sync <= 0;
 
         en_reg_out_7_0 <= 8'b0;
         en_reg_out_15_8 <= 8'b0;
@@ -46,23 +42,25 @@ always @(posedge clk or negedge rst_n) begin
         en_reg_pwm_15_8 <= 8'b0;
         pwm_duty_cycle <= 8'b0;
     end else begin
+        //capture on clock cycle
+        SCLK_sync <= {{SCLK_sync[SYNC-2]}{SCLK}};
+        COPI_sync <= {{SCLK_sync[SYNC-2]}{SCLK}};
+        nCS_sync <= {{SCLK_sync[SYNC-2]}{SCLK}};
         
-        sync_nCS <= {sync_nCS[SYNC-2:0], nCS};            //2'b10 is negedge
-        sync_SCLK <= {sync_SCLK[SYNC-2:0], SCLK};         //note, 2'b00 is lo, 2'b01 is posedge, 2'b11 is high, 2'b10 is negedge
-        sync_COPI <= {sync_COPI[SYNC-2:0], COPI};
-
-        if (sync_nCS == 2'b10) begin
-            sCLKcnt <= 5'b0;
-            data <= 16'b0;
-
-        end
-        else if (sync_nCS == 2'b00 && sync_SCLK == 2'b01) begin
-            if (sCLKcnt != 5'b10000) begin
-                data[15 - sCLKcnt] <= sync_COPI[SYNC-1];
-                sCLKcnt <= sCLKcnt + 1;
-            end
+        if(nCS_sync == 2'b10) begin //negedge. begin data capture
+            curr_bit <= 4'b0;
+            transaction_done <= 1'b0;
+            transaction_data <= 16'b0;
         end
 
+        else if (nCS_sync == 2'b00 && sync_SCLK == 2'b01) begin //posedge detect SCLK when data is valid
+            if (!transaction_done) begin
+                    transaction_data[15 - curr_bit] <= COPI_sync[SYNC-1];
+                    if(curr_bit + 1 == 4'b0) begin
+                        transaction_done <= 1'b1;
+                    end
+                    curr_bit <= curr_bit + 1;
+                end
         if(sCLKcnt == 5'b10000 && data[15] == 1'b1)begin
             case (data[14:8]) 
                 7'b0000000: en_reg_out_7_0 <= data[7:0];
@@ -74,7 +72,9 @@ always @(posedge clk or negedge rst_n) begin
                 //if address isnt one of them, just dont assign anything
             endcase
         end
+        end
     end
 end
+
 
 endmodule
